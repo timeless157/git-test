8e0b6e9a403a2d691ba960097eadd445d8325581
dong <1051346016@qq.com>
2022-10-13 12:52:13 +0800

    hot-fix commit1

--- src/main/java/com/timeless/git/GitTest.java
+++ src/main/java/com/timeless/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello,gitHub!");
         System.out.println("hello,gitee!");
+        System.out.println("hello,gitlab!");
     }
 }
